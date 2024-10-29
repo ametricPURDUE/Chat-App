9ddcf165d83afd53ce70e23bf29f64aa5c8904da
ametricPURDUE <ametric@purdue.edu>
2024-10-29 16:06:15 -0400

    Added all the starter files we should need

---
+++ src/src/ChatDatabase.java
@@ -0,0 +1,10 @@
+/**
+ * The database for our message app, handles the modification of users as well as other tasks
+ * Purdue University -- Fall 2024 -- CS18000 -- Team Project
+ *
+ * @author AJ Metrick, Edward Ju, Nolan Shultz, Parshawn Haynes, Pranav Sangani
+ * @version October 29, 2024
+ */
+public class ChatDatabase {
+
+}
---
+++ src/src/User.java
@@ -0,0 +1,20 @@
+/**
+ * The class that handles all functions relating to specific users
+ * Purdue University -- Fall 2024 -- CS18000 -- Team Project
+ *
+ * @author AJ Metrick, Edward Ju, Nolan Shultz, Parshawn Haynes, Pranav Sangani
+ * @version October 29, 2024
+ */
+public class User {
+    private String name;
+    private String username;
+    private User[] blocked = new User[0];
+    private User[] friends = new User[0];
+    private int age;
+
+    public User(String name, String username, int age) {
+        this.name = name;
+        this.username = username;
+        this.age = age;
+    }
+}
