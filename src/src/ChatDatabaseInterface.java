458fa1162239391edaa3e19c5612706181eac878
shultzn <shultzn@purdue.edu>
2024-11-03 12:48:55 -0500

    Update ChatDatabaseInterface.java
    Added  throws IncorrectInput to createUser

--- src/src/ChatDatabaseInterface.java
+++ src/src/ChatDatabaseInterface.java
@@ -2,7 +2,7 @@ public interface ChatDatabaseInterface {
     boolean readUsernames();
     boolean readPasswords();
     boolean writeUsernames();
-    boolean createUser(String data);
+    boolean createUser(String data) throws IncorrectInput;
     boolean removeUser(User user);
     boolean modifyUser(User user, String data) throws IncorrectInput;
 }
