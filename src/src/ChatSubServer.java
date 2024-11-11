5b250271d91c192936100e2a4bec6689faa0d9e6
shultzn <shultzn@purdue.edu>
2024-11-10 21:37:46 -0500

    Fixed server/client connection MainChatServer now correctly tracks busy/free ChatSubServers I think

--- src/src/ChatSubServer.java
+++ src/src/ChatSubServer.java
@@ -1,9 +1,28 @@
+import java.io.IOException;
+import java.net.*;
+
 class ChatSubServer implements Runnable {
     private int portNumber;
+    ServerSocket serverSocket;
+    private boolean running = false;
     public ChatSubServer(int portNumber) {
         this.portNumber = portNumber;
     }
     public void run() {
-
+        try {
+            serverSocket = new ServerSocket(portNumber);
+            Socket socket = serverSocket.accept();
+            running = true;
+            while(running) {
+                running = false;
+                running = true;
+            }
+            running = false;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public boolean portOpen() {
+        return !running;
     }
 }
--- src/src/MainChatServer.java
+++ src/src/MainChatServer.java
@@ -1,27 +1,35 @@
 import java.io.*;
 import java.net.*;
-import java.util.Arrays;
 
 public class MainChatServer{
     private static boolean[] emptyPorts = new boolean[10];
     public static void main(String[] args){
         try {
             Thread[] servers = new Thread[10];
+            ChatSubServer[] subservers = new ChatSubServer[10];
             // Creates 10 subservers for clients to connect to
             for (int i = 0; i < servers.length; i++) {
-                servers[i] = new Thread(new ChatSubServer(2000 + i));
+                subservers[i] = new ChatSubServer(2000 + i);
+                servers[i] = new Thread(subservers[i]);
                 servers[i].start();
             }
-            Arrays.fill(emptyPorts, true);
             ServerSocket serverSocket = new ServerSocket(4242); //Creating default serverSocket
             while (true) {
+                System.out.print("Open Ports:");
+                for (int i = 0; i < emptyPorts.length; i++) {
+                    emptyPorts[i] = subservers[i].portOpen();
+                    System.out.print(emptyPorts[i] + ",");
+                }
+                System.out.println();
                 String output = "";
                 boolean openPort = false;
                 int serverNumber = 2000;
                 Socket socket = serverSocket.accept(); //Accepts one client at a time
                 PrintWriter out = new PrintWriter(socket.getOutputStream()); // Creates output stream for the client
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Creates input stream to read from client
                 for (int i = 0; i < emptyPorts.length; i++) {
                     if(emptyPorts[i]){
+                        System.out.println(i);
                         serverNumber += i;
                         openPort = true;
                         output = serverNumber + ";" + i;
@@ -38,12 +46,22 @@ public class MainChatServer{
                     out.println();
                     out.flush();
                 }
+                String clientRecieved = reader.readLine();
+                if (clientRecieved.equals("NoServerFound")) {
+                    System.out.println("No open servers");
+                } else if (clientRecieved.equals("ReceivedServer")) {
+                    System.out.println("Client Received Server");
+                }
+                out.close();
+                reader.close();
+                socket.close();
+                for (int i = 0; i < emptyPorts.length; i++) {
+                    System.out.print(emptyPorts[i] + ",");
+                }
+                System.out.println();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-    public void updateEmptyServer(boolean b, int i) {
-        emptyPorts[i] = b;
     }
 }
