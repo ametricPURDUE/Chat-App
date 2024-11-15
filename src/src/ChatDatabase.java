f6c3c18117eb7a0ff07b4106898fc010b18b327f
Edward Ju <ju51@purdue.edu>
2024-11-15 18:44:09 -0500

    Update ChatDatabase.java

--- src/src/ChatDatabase.java
+++ src/src/ChatDatabase.java
@@ -38,14 +38,16 @@ public class ChatDatabase implements ChatDatabaseInterface {
      * @return - returns true if successful and false if not
      */
     public boolean readUsernames() {
-        try(BufferedReader bfr = new BufferedReader(new FileReader("usernames.txt"))) {
-            String line;
-            while ((line = bfr.readLine()) != null) {
-                usernames.add(line);
+        synchronized(MAIN_LOCK) {
+            try(BufferedReader bfr = new BufferedReader(new FileReader("usernames.txt"))) {
+                String line;
+                while ((line = bfr.readLine()) != null) {
+                    usernames.add(line);
+                }
+                return true;
+            } catch (IOException e) {
+                return false;
             }
-            return true;
-        } catch (IOException e) {
-            return false;
         }
     }
 
@@ -54,14 +56,16 @@ public class ChatDatabase implements ChatDatabaseInterface {
      * @return - returns true if successful and false if not
      */
     public boolean readPasswords() {
-        try (BufferedReader bfr = new BufferedReader(new FileReader("passwords.txt"))) {
-            String line;
-            while ((line = bfr.readLine()) != null) {
-                passwords.add(line);
+        synchronized(MAIN_LOCK) {
+            try (BufferedReader bfr = new BufferedReader(new FileReader("passwords.txt"))) {
+                String line;
+                while ((line = bfr.readLine()) != null) {
+                    passwords.add(line);
+                }
+                return true;
+            } catch (IOException e) {
+                return false;
             }
-            return true;
-        } catch (IOException e) {
-            return false;
         }
     }
     //set of write functions;
@@ -70,14 +74,16 @@ public class ChatDatabase implements ChatDatabaseInterface {
      * @return - returns true if successful and false if not
      */
     public boolean writeUsernames() {
-        try (BufferedWriter bfw = new BufferedWriter(new FileWriter("usernames.txt"))) {
-            for(String username: usernames) {
-                bfw.write(username);
-                bfw.newLine();
+        synchronized(MAIN_LOCK) {
+            try (BufferedWriter bfw = new BufferedWriter(new FileWriter("usernames.txt"))) {
+                for(String username: usernames) {
+                    bfw.write(username);
+                    bfw.newLine();
+                }
+                return true;
+            } catch (IOException e) {
+                return false;
             }
-            return true;
-        } catch (IOException e) {
-            return false;
         }
     }
 
@@ -89,45 +95,51 @@ public class ChatDatabase implements ChatDatabaseInterface {
      */
 
     public static boolean writePasswords() {
-        try (BufferedWriter bfw = new BufferedWriter(new FileWriter("passwords.txt"))) {
-            for(String password: passwords) {
-                bfw.write(password);
-                bfw.newLine();
+        synchronized(MAIN_LOCK) {
+            try (BufferedWriter bfw = new BufferedWriter(new FileWriter("passwords.txt"))) {
+                for(String password: passwords) {
+                    bfw.write(password);
+                    bfw.newLine();
+                }
+                return true;
+            } catch (IOException e) {
+                return false;
             }
-            return true;
-        } catch (IOException e) {
-            return false;
         }
     }
 
     // Updates the password in the passwords list for a given username.
     public static boolean updatePassword(String username, String newPassword) {
-        int index = usernames.indexOf(username);
-        if (index != -1) {
-            passwords.set(index, newPassword);
-            writePasswords();
-            return true;
+        synchronized(MAIN_LOCK) {
+            int index = usernames.indexOf(username);
+            if (index != -1) {
+                passwords.set(index, newPassword);
+                writePasswords();
+                return true;
+            }
+            return false;
         }
-        return false;
     }
 
     public boolean createUser(String data) throws IncorrectInput {
-        String[] userData = data.split(",");
-        if (userData.length < 3) {
-            throw new IncorrectInput("Input string data formatted incorrectly");
+        synchronized(MAIN_LOCK) {
+            String[] userData = data.split(",");
+            if (userData.length < 3) {
+                throw new IncorrectInput("Input string data formatted incorrectly");
+            }
+            int newAge = -1;
+            try {
+                newAge = Integer.parseInt(userData[2]);
+            } catch (NumberFormatException e) {
+                throw new IncorrectInput("Please input a number for age");
+            }
+            if (newAge == -1) {
+                throw new IncorrectInput("Age cannot be below 0");
+            }
+            userList.add(new User(userData[0], userData[1], newAge));
+            usernames.add(userData[1]);
+            return true;
         }
-        int newAge = -1;
-        try {
-            newAge = Integer.parseInt(userData[2]);
-        } catch (NumberFormatException e) {
-            throw new IncorrectInput("Please input a number for age");
-        }
-        if (newAge == -1) {
-            throw new IncorrectInput("Age cannot be below 0");
-        }
-        userList.add(new User(userData[0], userData[1], newAge));
-        usernames.add(userData[1]);
-        return true;
     }
 
     public boolean removeUser(User user) {
@@ -155,70 +167,76 @@ public class ChatDatabase implements ChatDatabaseInterface {
      * @return - returns true user is found and modified and false if not
      */
     public boolean modifyUser(User user, String data) throws IncorrectInput{
-        String[] parts = data.split(",");
-        if (parts.length != 4) {
-            return false;
-        }
-        String name = parts[0];
-        int age = -1;
-        try {
-            age = Integer.parseInt(parts[1]);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        String username = parts[2];
-        String password = parts[3];
-        User newUser = new User(name, username, age);
-        for (int i = 0; i < userList.size(); i++) {
-            if (userList.get(i).equals(user)) {
-                userList.set(i, newUser);
-                usernames.set(i, username);
-                passwords.set(i, password);
-                return true;
+        synchronized(MAIN_LOCK) {
+            String[] parts = data.split(",");
+            if (parts.length != 4) {
+                return false;
             }
+            String name = parts[0];
+            int age = -1;
+            try {
+                age = Integer.parseInt(parts[1]);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+            String username = parts[2];
+            String password = parts[3];
+            User newUser = new User(name, username, age);
+            for (int i = 0; i < userList.size(); i++) {
+                if (userList.get(i).equals(user)) {
+                    userList.set(i, newUser);
+                    usernames.set(i, username);
+                    passwords.set(i, password);
+                    return true;
+                }
+            }
+            return false;
         }
-        return false;
     }
 
     public boolean writeMessageToFile(String senderUsername, String receiverUsername, String message) {
-        String filename;
-        if (senderUsername.compareTo(receiverUsername) < 0) {
-            filename = "chat" + senderUsername + receiverUsername + ".txt";
-        } else {
-            filename = "chat" + receiverUsername + senderUsername + ".txt";
-        }
-
-        try (PrintWriter writer = new PrintWriter(new FileWriter(filename, true))) {
-            writer.println(senderUsername + ": " + message);
-            return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-
-        public ArrayList<String> readMessagesFromFile(String senderUsername, String receiverUsername) {
+        synchronized(MAIN_LOCK) {
             String filename;
             if (senderUsername.compareTo(receiverUsername) < 0) {
                 filename = "chat" + senderUsername + receiverUsername + ".txt";
             } else {
                 filename = "chat" + receiverUsername + senderUsername + ".txt";
             }
-
-            ArrayList<String> messages = new ArrayList<>();
-
-            try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    messages.add(line);
-                }
-            } catch (FileNotFoundException e) {
-                System.out.println("No previous conversation found between " + senderUsername + " and " + receiverUsername);
+    
+            try (PrintWriter writer = new PrintWriter(new FileWriter(filename, true))) {
+                writer.println(senderUsername + ": " + message);
+                return true;
             } catch (IOException e) {
                 e.printStackTrace();
+                return false;
             }
-            return messages;
+        }
+    }
+
+
+        public ArrayList<String> readMessagesFromFile(String senderUsername, String receiverUsername) {
+            synchronized(MAIN_LOCK) {
+                String filename;
+                if (senderUsername.compareTo(receiverUsername) < 0) {
+                    filename = "chat" + senderUsername + receiverUsername + ".txt";
+                } else {
+                    filename = "chat" + receiverUsername + senderUsername + ".txt";
+                }
+    
+                ArrayList<String> messages = new ArrayList<>();
+    
+                try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        messages.add(line);
+                    }
+                } catch (FileNotFoundException e) {
+                    System.out.println("No previous conversation found between " + senderUsername + " and " + receiverUsername);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return messages;
+            }
         }
     }
 
